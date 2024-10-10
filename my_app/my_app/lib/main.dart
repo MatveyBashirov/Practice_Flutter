eb13613a8fcd3322dd326e7a610b00c147af4e02
Matvey Bashirov <matvejbasirov333@gmail.com>
2024-10-10 17:33:37 +0300

    Images added

--- my_app/my_app/lib/main.dart
+++ my_app/my_app/lib/main.dart
@@ -2,36 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      body: MyClass()
-    ),
+    home: Scaffold(body: MyClass()),
   ));
 }
 
-class MyClass extends StatefulWidget{
+class MyClass extends StatefulWidget {
   MyClass({super.key});
   @override
   _MyClassState createState() => _MyClassState();
-  }
+}
 
 class _MyClassState extends State<MyClass> {
-
   //Варианты переменных для изменения по нажатию
   String currentText = "Хотите узнать, как вы выглядите?";
   List<String> endings = [" чудесно!", " волшебно!", " шикарно!"];
+  List<String> images = ["assets/images/cat1.jpeg", "assets/images/cat2.jpg", "assets/images/cat3.jpg"];
   int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {setState(() {
-        index = (index + 1) % endings.length; // Циклический переход
-        currentText = "Вы выглядите${endings[index]}";
-      });  },
-      child: Text(
-        currentText,
-        style: TextStyle(fontSize: 34)
-      )
-    );
+    return Column(children: [
+      Image.asset(images[index]),
+      ElevatedButton(
+          onPressed: () {
+            setState(() {
+              index = (index + 1) % endings.length; // Циклический переход
+              currentText = "Вы выглядите${endings[index]}";
+            });
+          },
+          child: Text(currentText, style: TextStyle(fontSize: 34)))
+    ]);
   }
 }
