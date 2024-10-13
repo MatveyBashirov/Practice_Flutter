70bced88986997e5a642a7873010cb8efa42933c
Matvey Bashirov <matvejbasirov333@gmail.com>
2024-10-13 09:02:20 +0300

    remove code from main.dart

--- my_app/my_app/lib/main.dart
+++ my_app/my_app/lib/main.dart
@@ -1,49 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/lookcheck.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(body: MyClass()),
   ));
-}
-
-class MyClass extends StatefulWidget {
-  MyClass({super.key});
-  @override
-  _MyClassState createState() => _MyClassState();
-}
-
-class _MyClassState extends State<MyClass> {
-  //Варианты переменных для изменения по нажатию
-  String currentText = "Хотите узнать, как вы выглядите?";
-  List<String> endings = [" чудесно!", " волшебно!", " шикарно!"];
-  List<String> images = [
-    "assets/images/cat1.jpeg",
-    "assets/images/cat2.jpg",
-    "assets/images/cat3.jpg"
-  ];
-  List<String> fonts = ['TrueType', 'OpenType', 'TenorSans'];
-  int index = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-          Padding(
-            padding: EdgeInsets.all(30),
-            child: Image.asset(images[index]),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  index = (index + 1) % endings.length; // Циклический переход
-                  currentText = "Вы выглядите${endings[index]}";
-                });
-              },
-              child: Text(currentText,
-                  style: TextStyle(fontFamily: fonts[index], fontSize: 24)))
-        ]));
-  }
 }
