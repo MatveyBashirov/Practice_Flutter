32aec3e783ce46c7d8f45360b9077cb81f6fc217
Matvey Bashirov <matvejbasirov333@gmail.com>
2024-10-16 18:01:35 +0300

    form with controllers

--- my_app/my_app/lib/daycheck.dart
+++ my_app/my_app/lib/daycheck.dart
@@ -12,37 +12,69 @@ class DaycheckFormState extends State<DaycheckForm>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor:Color.fromARGB(255, 115, 132, 217),
+        foregroundColor: Color.fromARGB(255, 255, 255, 255),
         title: Center(
           child: Text('Опросник'),
         ),
       ),
-      body: Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Row(children: [
-            Expanded(
-              child: TextFormField(
-                validator: (value){
-                  if (value == null || value.isEmpty) {
-                  return 'Введите данные';
-                  }
-                  return null;
-                },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+        child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Row(children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Как опишите день одним словом?',
+                    ),
+                    validator: (value){
+                      if (value == null || value.isEmpty) {
+                      return 'Введите данные';
+                      }
+                      return null;
+                    },
+                  ),
+                )
+              ]
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Row(children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Поставьте оценку дню по 10-бальной шкале',
+                    ),
+                    validator: (value){
+                      if (value == null || value.isEmpty) {
+                      return 'Введите данные';
+                      }
+                      return null;
+                    },
+                  ),
+                )
+              ]
+              ),
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: submitForm,
+                  child: Text('Сохранить'))
+              ],
             )
-          ]
-          ),
-          Row(
-            children: [
-              ElevatedButton(
-                onPressed: submitForm,
-                child: Text('Сохранить'))
-            ],
-          )
-        ],
+          ],
+        ),
+            ),
       ),
-    ),
     );
   }
 
