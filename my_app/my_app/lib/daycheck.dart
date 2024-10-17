c101e03a38f040447e02f656c326bc4918740b26
Matvey Bashirov <matvejbasirov333@gmail.com>
2024-10-17 17:52:07 +0300

    snackbar added

--- my_app/my_app/lib/daycheck.dart
+++ my_app/my_app/lib/daycheck.dart
@@ -6,8 +6,7 @@ class DaycheckForm extends StatefulWidget {
   State<DaycheckForm> createState() => DaycheckFormState();
 }
 
-class DaycheckFormState extends State<DaycheckForm>{
-
+class DaycheckFormState extends State<DaycheckForm> {
   final _formKey = GlobalKey<FormState>();
   final formWordOfDay = TextEditingController();
   final formGrade = TextEditingController();
@@ -15,7 +14,7 @@ class DaycheckFormState extends State<DaycheckForm>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:Color.fromARGB(255, 115, 132, 217),
+        backgroundColor: Color.fromARGB(255, 115, 132, 217),
         foregroundColor: Color.fromARGB(255, 255, 255, 255),
         title: Center(
           child: Text('Опросник'),
@@ -24,70 +23,76 @@ class DaycheckFormState extends State<DaycheckForm>{
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
         child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Row(children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: formWordOfDay,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Как опишите день одним словом?',
+          key: _formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: formWordOfDay,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Как опишите день одним словом?',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Введите данные';
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value){
-                      if (value == null || value.isEmpty) {
-                      return 'Введите данные';
-                      }
-                      return null;
-                    },
-                  ),
-                )
-              ]
+                  )
+                ]),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Row(children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: formGrade,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
-                    ],
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Поставьте оценку дню по 10-бальной шкале',
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Row(children: [
+                  Expanded(
+                    child: DropdownButtonFormField<int>(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Поставьте оценку дню по 10-бальной шкале',
+                      ),
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Введите данные';
+                        }
+                        return null;
+                      },
+                      items: List.generate(10, (index) {
+                        return DropdownMenuItem<int>(
+                          value: index + 1,
+                          child: Text('${index + 1}'),
+                        );
+                      }),
+                      onChanged: (Object? value) {},
                     ),
-                    validator: (value){
-                      if (value == null || value.isEmpty) {
-                      return 'Введите данные';
-                      }
-                      return null;
-                    },
-                  ),
-                )
-              ]
+                  )
+                ]),
               ),
-            ),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: submitForm,
-                  child: Text('Сохранить'))
-              ],
-            )
-          ],
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: submitForm,
+                      child: Text('Сохранить'))
+                ],
+              )
+            ],
+          ),
         ),
-            ),
       ),
     );
   }
 
-  void submitForm(){
-    if(_formKey.currentState?.validate()??false){}
+  void submitForm() {
+    if (_formKey.currentState?.validate() ?? false) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Данные успешно сохранены!'),
+        ),
+      );
+    }
   }
-
 }
